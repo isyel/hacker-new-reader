37b3f61ea9298427dd1aaceabd91f32c77bf0212
isyel <isykaal4saviour@gmail.com>
2021-10-04 22:22:17 +0100

    🔗 update dynamic link to static - this is just for initial deploy

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-	? "http://localhost:3000"
-	: "https://jobs-listing-interface.vercel.app";
+export const server = "http://localhost:3000";
